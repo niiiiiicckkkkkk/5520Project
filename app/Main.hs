73928433cdaaa22c53d6a1bb0e5fa8c1478679fb
nicklee <nicklee@seas.upenn.edu>
2023-12-18 11:15:33 -0500

    arbitrary stores

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import Control.Monad.State (StateT, evalStateT, runStateT)
+import Control.Monad (foldM)
+import Control.Monad.State (StateT, evalStateT, execStateT, runStateT)
 import Control.Monad.State qualified as S
+import Data.List ((!!))
 import Data.List qualified as List
 import Data.Maybe (fromMaybe)
 import LuParser (parseLuExp, parseLuFile, parseStatement)
@@ -20,6 +22,7 @@ import LuStepper
 import LuSyntax
 import LuSyntax (Block (Block), pretty)
 import System.IO (BufferMode (NoBuffering), hSetBuffering, stdout)
+import Test.QuickCheck (Gen, arbitrary, sample, sample')
 import Test.QuickCheck qualified as QC
 import Text.Read (readMaybe)
 
@@ -31,27 +34,39 @@ instance QC.Arbitrary Store where
     y <- S.execStateT x initialStore
     return y -}
 
-{-QC.sized $ aux initialStore
-where
-  aux :: Store -> Int -> QC.Gen Store
-  aux store n =
-    QC.frequency
-      [ (1, return store),
-        ( n,
-          do
-            v <- (QC.arbitrary :: QC.Gen Value)
-            s <- genStringLit
-            -- IO ((), store)
-            (x, st) <- S.runStateT (update (Ref s) v) store
-            aux st (n `div` 2)
-        )
-      ] -}
+instance QC.Arbitrary (StateT Store IO Store) where
+  arbitrary :: QC.Gen (StateT Store IO Store)
+  arbitrary = do
+    len <- QC.chooseInt (0, 200)
+    vals <- QC.vectorOf len (QC.arbitrary :: QC.Gen Value)
+    refs <- QC.vectorOf len (Ref <$> genStringLit)
+    let entries = zip refs vals
+    return $ foldM f initialStore entries
+
+f :: Store -> (Reference, Value) -> StateT Store IO Store
+f s (r, v) = update r v >> S.get >>= \s' -> return s'
+
+-- >>> sample $ (arbitrary :: Gen (StateT Store IO Store))
+-- No instance for (Show (StateT Store IO Store))
+--   arising from a use of `sample'
+-- In the first argument of `($)', namely `sample'
+-- In the expression:
+--   sample $ (arbitrary :: Gen (StateT Store IO Store))
+-- In an equation for `it_a1q0QO':
+--     it_a1q0QO = sample $ (arbitrary :: Gen (StateT Store IO Store))
 
 main :: IO ()
 main = do
-  hSetBuffering stdout NoBuffering
-  evalStateT go initialStore
+  -- hSetBuffering stdout NoBuffering
+  -- evalStateT go initialStore
+  -- return ()
 
--- ls <- QC.sample' (QC.arbitrary :: QC.Gen Store)
--- print $ map (\s -> (pretty $ globalstore s)) ls
+  ls <- QC.sample' (QC.arbitrary :: QC.Gen (StateT Store IO Store))
+  ls' <- mapM (\st -> S.runStateT st initialStore) ls
+  mapM (\(_, s) -> putStrLn (pretty $ globalstore s)) ls'
+  return ()
+
+-- print $ map (\state -> aux state) ls
+-- (_, s) <- S.runStateT (head ls) initialStore
+-- putStrLn (pretty $ globalstore s)
 -- return ()
